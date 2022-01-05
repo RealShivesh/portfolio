6aa20fdbfebfd42c116a9d4c04be79da8274f45e
Shivesh Sinha <realshivesh@gmail.com>
2022-01-05 19:44:28 +0530

    adds country database project

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -9,6 +9,22 @@ export const Projects = () => {
     <>
       <Page>
         <h1>Projects</h1>
+        <article>
+          <h2>Country database using JavaScript</h2>
+          <a
+            href="http://realshivesh.github.io/countries"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Country database using Vanilla JavaScript
+          </a>
+          <div>
+            This is a country section which contains various information about
+            different countries, their capital, language, etc. I will be
+            upgrading this project to react as well, but for now, let's stick to
+            vanilla.
+          </div>
+        </article>
       </Page>
     </>
   )
