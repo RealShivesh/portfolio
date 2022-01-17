05bab10cc7d875c11b30973a98d743f5d62fd419
Shivesh Sinha <realshivesh@gmail.com>
2022-01-17 10:52:05 +0530

    creates a mapping function for projects

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,5 +1,6 @@
 import { DemoLink, Heading, Page, Project } from '../styles/Main.styles'
 import { Description, Projects as List } from '../styles/Projects.styles'
+import { ProjectList } from '../data/ProjectList'
 
 export const Projects = () => {
   return (
@@ -7,40 +8,17 @@ export const Projects = () => {
       <Page>
         <Heading>Projects</Heading>
         <List>
-          <Project>
-            <h2>Country database using JavaScript</h2>
-            <div>
-              <DemoLink
-                href="http://realshivesh.github.io/countries"
-                target="_blank"
-                rel="noreferrer"
-              >
-                Live Demo
-              </DemoLink>
-              <Description>
-                This is a country section which contains various information
-                about different countries, their capital, language, etc. I will
-                be upgrading this project to react as well, but for now, let's
-                stick to vanilla.
-              </Description>
-            </div>
-          </Project>
-          <Project>
-            <h2>Weather App using React and Styled Components</h2>
-            <div>
-              <DemoLink
-                href="http://realshivesh.github.io/weather-pwa"
-                target="_blank"
-                rel="noreferrer"
-              >
-                Live Demo
-              </DemoLink>
-              <Description>
-                This is a weather app which has been created using React and
-                Styled components.
-              </Description>
-            </div>
-          </Project>
+          {ProjectList.map((project) => (
+            <Project key={project.id}>
+              <h2>{project.name}</h2>
+              <div>
+                <DemoLink href={project.link} target="_blank" rel="noreferrer">
+                  Live Demo
+                </DemoLink>
+                <Description>{project.description}</Description>
+              </div>
+            </Project>
+          ))}
         </List>
       </Page>
     </>
---
+++ src/data/ProjectList.js
@@ -0,0 +1,28 @@
+export const ProjectList = [
+  {
+    id: 0,
+    name: 'Countrydex',
+    link: 'http://realshivesh.github.io/countries',
+    description:
+      "This is a country section which contains various information about different countries, their capital, language, etc. I will be upgrading this project to react as well, but for now, let's stick to vanilla.",
+  },
+  {
+    id: 1,
+    name: 'Weather App',
+    link: 'http://realshivesh.github.io/countries',
+    description:
+      'This is a weather app which has been created using React and Styled components',
+  },
+  {
+    id: 2,
+    name: 'Budget Tracker',
+    link: 'http://realshivesh.github.io/countries',
+    description: 'This is a budget tracking app using the CRUD principle.',
+  },
+  {
+    id: 3,
+    name: 'To-do list',
+    link: 'http://realshivesh.github.io/countries',
+    description: 'A simple to-do list app using basics of react',
+  },
+]
