8647d0b7a8ff332b69855026e337e296db3d734a
Shivesh Sinha <realshivesh@gmail.com>
2022-05-18 20:19:24 +0530

    basic updates - adds header, hero and projects components - enables prettierrc file

---
+++ components/Hero.js
@@ -0,0 +1,18 @@
+export const Hero = () => {
+    return (
+        <div>
+            <section>
+                <h1>
+                    Welcome to <br />
+                    <span>RealShivesh&apos;s</span> <br />
+                    <span>Portfolio</span>
+                </h1>
+                <div>
+                    I try to solve problems using <br />
+                    codes and technologies.
+                </div>
+                <button>Download resume</button>
+            </section>
+        </div>
+    )
+}
---
+++ styles/Header.styles.js
@@ -0,0 +1,8 @@
+import styled from 'styled-components'
+
+export const Name = styled.a`
+    font-size: 1.5rem;
+    font-weight: bold;
+    text-align: center;
+    margin: 0 auto;
+`
