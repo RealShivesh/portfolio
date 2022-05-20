4181ea938b0cd4b2a7de4591e103d8c30716bf96
Shivesh Sinha <realshivesh@gmail.com>
2022-05-20 14:24:34 +0530

    removes location and replaced with an input bar

--- components/WeatherCard.js
+++ components/WeatherCard.js
@@ -4,68 +4,78 @@ import { Card } from '../styles/Globals.styles'
 
 const WeatherCard = () => {
     const [weatherData, setWeatherData] = useState({})
+    const [query, setQuery] = useState('')
 
-    const location = 'Mumbai, IN'
     //get current location coordinates
-    const getLocation = () => {
-        if (navigator.geolocation) {
-            navigator.geolocation.watchPosition(
-                (position) => {
-                    const { latitude, longitude } = position.coords
-                    fetchWeather(`${latitude},${longitude}`)
-                        .then((data) => {
-                            setWeatherData(data)
-                            console.log(data)
-                        })
-                        .catch((err) => {
-                            console.log(err)
-                        })
-                },
-                (err) => {
-                    console.log(err)
-                }
-            )
-        } else {
-            console.log('Geolocation is not supported by this browser.')
+    // const getLocation = () => {
+    //     if (navigator.geolocation) {
+    //         navigator.geolocation.getCurrentLocation(
+    //             (position) => {
+    //                 const { latitude, longitude } = position.coords
+    //                 fetchWeather(`${latitude},${longitude}`)
+    //                     .then((data) => {
+    //                         setWeatherData(data)
+    //                         console.log(data)
+    //                     })
+    //                     .catch((err) => {
+    //                         console.log(err)
+    //                     })
+    //             },
+    //             (err) => {
+    //                 console.log(err)
+    //             }
+    //         )
+    //     } else {
+    //         console.log('Geolocation is not supported by this browser.')
+    //     }
+    // }
+
+    const search = async (e) => {
+        if (e.key === 'Enter') {
+            const data = await fetchWeather(query)
+            console.log(data)
+            setWeatherData(data)
+            //setQuery('')
         }
     }
 
-    useEffect(() => {
-        getLocation()
-    }, [])
-
     return (
         <div>
-            <h3>Weather of the place</h3>
-
+            <input
+                type="text"
+                placeholder="Enter your favourite city"
+                value={query}
+                onChange={(e) => setQuery(e.target.value)}
+                onKeyPress={search}
+            />
             <div>
                 {weatherData.current && (
-                    <Card>
-                        <div>
+                    <div>
+                        <Card>
                             <span>{weatherData.current.temp_c}</span>
                             <span>&deg;C</span>
-                        </div>
-                        <div>
-                            <div>{weatherData.current.condition.text}</div>
                             <div>
-                                {weatherData.location.name},{' '}
-                                {weatherData.location.region},{' '}
-                                {weatherData.location.country}
+                                <div>{weatherData.current.condition.text}</div>
+                                <div>
+                                    {weatherData.location.name},{' '}
+                                    {weatherData.location.region},{' '}
+                                    {weatherData.location.country}
+                                </div>
                             </div>
-                        </div>
+                        </Card>
                         <>
                             Forecast
                             {weatherData.forecast.forecastday.map((day) => (
-                                <div key={day.date}>
+                                <Card key={day.date}>
                                     <span>{day.date}</span>
                                     <span>{day.day.condition.text}</span>
                                     <span>{day.day.avgtemp_c}&deg;C</span>
                                     <span>{day.day.maxtemp_c}&deg;C</span>
                                     <span>{day.day.mintemp_c}&deg;C</span>
-                                </div>
+                                </Card>
                             ))}
                         </>
-                    </Card>
+                    </div>
                 )}
             </div>
         </div>
